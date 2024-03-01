8afd5fd5bed21982735baa07dc11cd5b470e2b37
Becky Soll <beckysoll@gmail.com>
2024-03-01 17:54:05 -0500

    tabbing skips submenus unless theyre open

--- js/bustime.js
+++ js/bustime.js
@@ -19,6 +19,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
           // Set the height property to achieve animation
           subMenu.style.maxHeight = parent.classList.contains('open') ? actualHeight + 'px' : '0';
+
+          // Toggle tabindex of <a> or <button> elements within .sub-menu
+          var innerTabbableItems = subMenu.querySelectorAll('a[tabindex], button[tabindex]');
+          innerTabbableItems.forEach(function(element) {
+            var currentIndex = element.getAttribute('tabindex');
+            element.setAttribute('tabindex', currentIndex === '0' ? '-1' : '0');
+          });
         }
       }
     });
