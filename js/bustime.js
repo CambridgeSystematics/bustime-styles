e1e259ed4397e0ff7177986bd7d98e8c6875b189
Becky Soll <beckysoll@gmail.com>
2024-03-07 12:39:06 -0500

    aria tweaks

--- js/bustime.js
+++ js/bustime.js
@@ -38,17 +38,19 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   // map trigger open and close
-  var mapTrigger = document.getElementById('map-trigger');
+  var mapToggle = document.getElementById('map-toggle');
   var map = document.getElementById('map');
+  var mapWrap = document.getElementById('map-wrap');
 
-  mapTrigger.addEventListener('click', function() {
-    mapWrap = document.getElementById('map-wrap');
+  mapToggle.addEventListener('click', function() {
+
+    window.console.log('mapToggle clicked');
     
     mapWrap.classList.toggle('open');
 
-    mapTrigger.setAttribute('aria-expanded', map.classList.contains('open'));
-    mapTrigger.setAttribute('aria-label', map.classList.contains('open') ? 'Close Map' : 'Open Map');
-    mapTrigger.setAttribute('aria-pressed', map.classList.contains('open'));
+    mapToggle.setAttribute('aria-expanded', mapWrap.classList.contains('open'));
+    mapToggle.setAttribute('aria-label', mapWrap.classList.contains('open') ? 'Toggle Map Visibility (currently visible)' : 'Toggle Map Visibility (currently hidden)');
+    mapToggle.setAttribute('aria-pressed', mapWrap.classList.contains('open'));
   });
 
 
